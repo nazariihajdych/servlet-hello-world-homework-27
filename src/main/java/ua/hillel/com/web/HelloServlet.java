6bbaf8c83b2522713b2b679d1628abd1fce4255c
Nazarii Hajdych <wertyp.poiteq@gmail.com>
2023-08-22 18:27:22 +0300

    homework 27

---
+++ src/main/java/ua/hillel/com/web/HelloServlet.java
@@ -0,0 +1,28 @@
+package ua.hillel.com.web;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("Hello word!");
+        resp.getWriter().flush();
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
+}
